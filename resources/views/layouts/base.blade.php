2bb7a83646a297dfca1698052f79c89944ca57c9
nox <10474785+nox-4O4@users.noreply.github.com>
2025-02-09 17:56:18 +0100

    Minor design adjustments

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,6 +9,7 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="AStA-Mietportal" />
+    <meta name="theme-color" content="#f0f0ef" />
     <link rel="manifest" href="/site.webmanifest" />
 
     <title>{{ config('app.name') }}{{ isset($title) ? " - $title" : '' }}</title>
@@ -17,14 +18,13 @@
 </head>
 <body>
 @yield('content')
-
-<div class="position-absolute text-right p-3 w-100" style="top:70px;left:0;z-index: 999999">
-    <p class="d-sm-none">[none]</p>
-    <p class="d-none d-sm-block d-md-none">sm</p>
-    <p class="d-none d-md-block d-lg-none">md</p>
-    <p class="d-none d-lg-block d-xl-none">lg</p>
-    <p class="d-none d-xl-block d-xxl-none">xl</p>
-    <p class="d-none d-xxl-block">xxl</p>
+<div class="position-fixed text-right p-3" style="top:70px;right:0;z-index: 999999">
+    <p class="m-0 d-sm-none">[none]</p>
+    <p class="m-0 d-none d-sm-block d-md-none">sm</p>
+    <p class="m-0 d-none d-md-block d-lg-none">md</p>
+    <p class="m-0 d-none d-lg-block d-xl-none">lg</p>
+    <p class="m-0 d-none d-xl-block d-xxl-none">xl</p>
+    <p class="m-0 d-none d-xxl-block">xxl</p>
 </div>
 </body>
 </html>
