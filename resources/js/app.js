568400164927f465c09372889c1c81809f641631
nox <10474785+nox-4O4@users.noreply.github.com>
2025-02-09 17:55:29 +0100

    Add dynamic dark mode (manual toggle still missing)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,19 @@
 import * as bootstrap from 'bootstrap'
 import CreateDataTable from './DataTableFactory.js';
-
-export * from './LivewireExtensions'
-
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
+
+export * from './LivewireExtensions'
 
 window.CreateDataTable = CreateDataTable
 window.bootstrap = bootstrap
 window.Swiper = Swiper
+
+;(() => {
+    const updateTheme = () => {
+        const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        document.querySelector("html").setAttribute("data-bs-theme", colorMode);
+    }
+    updateTheme()
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+})()
