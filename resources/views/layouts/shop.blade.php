34aaab5722c6633e78de09d40f63a0bfa5b41b83
nox <10474785+nox-4O4@users.noreply.github.com>
2026-06-21 14:31:57 +0200

    Add dashboard button to topbar when logged in

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -5,21 +5,31 @@
 @section('content')
     <div class="d-flex flex-column min-vh-100 shop">
         <div class="sticky-top bg-light-subtle shadow">
-            <div class="shop-content mx-auto p-3 p-lg-4 d-flex justify-content-between align-items-stretch">
+            <div class="shop-content shop-topbar mx-auto p-3 p-lg-4 d-flex justify-content-between align-items-stretch">
                 <a class="logo-small text-body" href="{{route('shop')}}" wire:navigate title="Zur Startseite des AStA-Mietportals">
                     {!! File::get(resource_path('img/logo-asta.svg')) !!}
                 </a>
                 <div class="flex-grow-1 px-3 search-bar">
                     <livewire:shop.search-form />
                 </div>
-                {{-- Persisting the cart-badge component between navigation prevents a race condition during checkout.
-                     If the success page loads prior to the (old) card bage being updated, the local storage cart items are not yet cleared as this happens only when the old cart bage is updated.
-                     The success page then renders it's own (new) cart bage component, which leads to $wire.persist updating the cart repository with the (old) local storage data.
-                     By having the cart badge component persisted between page loads, the success page won't render a new cart bage component. It will instead use the existing one,
-                     where the update will lead to the local storage cart items being cleared. --}}
-                @persist('cart-badge')
-                <livewire:shop.cart-badge />
-                @endpersist
+                <div class="d-flex gap-md-3 gap-1">
+                    @auth
+                        <a class="h-100 d-flex align-items-center dashboard-button" href="{{route(config('shop.dashboard.defaultRoute'))}}" title="Dashboard" wire:navigate>
+                            <span class="me-1 d-none d-md-inline">Dashboard</span>
+                            <span class="fa-stack fa-lg h-100 text-body">{{-- fa-stack is only used to ensure same look and sizing as cart badge icon --}}
+                                <i class="fa-solid fa-table-list fa-stack-1x fa-xl"></i>
+                            </span>
+                        </a>
+                    @endauth
+                    {{-- Persisting the cart-badge component between navigation prevents a race condition during checkout.
+                         If the success page loads prior to the (old) card bage being updated, the local storage cart items are not yet cleared as this happens only when the old cart bage is updated.
+                         The success page then renders it's own (new) cart bage component, which leads to $wire.persist updating the cart repository with the (old) local storage data.
+                         By having the cart badge component persisted between page loads, the success page won't render a new cart bage component. It will instead use the existing one,
+                         where the update will lead to the local storage cart items being cleared. --}}
+                    @persist('cart-badge')
+                    <livewire:shop.cart-badge />
+                    @endpersist
+                </div>
             </div>
         </div>
         @php($disabledDate = DisabledDate::getOverlappingRanges(\Carbon\CarbonImmutable::now()) ->first(fn(DisabledDate $d) => trim($d->site_notice) != ''))
@@ -52,11 +62,11 @@
                         <a href="https://asta-hka.de/impressum/" target="_blank">Impressum</a>
                     </div>
                     <div class="col-md my-1 my-md-0">
-                        @if(auth()->check())
+                        @auth
                             <a href="{{route(config('shop.dashboard.defaultRoute'))}}" wire:navigate>Dashboard</a>
                         @else
                             <a href="{{route('login')}}" wire:navigate>Login</a>
-                        @endif
+                        @endauth
                     </div>
                 </div>
             </div>
